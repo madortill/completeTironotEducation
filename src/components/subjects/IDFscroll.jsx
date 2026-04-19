28cc3152c45014034278c7e3ed215e5c9941e4e7
madortill <mador.till@gmail.com>
2026-04-19 17:41:31 +0300

    19/4

--- src/components/subjects/IDFscroll.jsx
+++ src/components/subjects/IDFscroll.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import nextBtn from "../../assets/images/introduction/nextBtn.png";
 import backBtn from "../../assets/images/introduction/backBtn.png";
 import ScrollWithStars from "../../components/ScrollWithStars";
+import Pazzle from "../../components/Pazzle";
 import "../../css/IDFscroll.css";
 
 import closeScroll from "../../assets/images/IDFscroll/closeScroll.png";
@@ -11,6 +12,7 @@ import goal from "../../assets/images/meetEducation/goal.png";
 function IDFscroll({ page, setPage, goToPrevSubject }) {
   const [showStars, setShowStars] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
+  const [isOpenGoal, setIsOpenGoal] = useState(false);
 
   useEffect(() => {
     if (page === 0) {
@@ -66,18 +68,40 @@ function IDFscroll({ page, setPage, goToPrevSubject }) {
           <p className="title-content">מהי מגילת צה"ל?</p>
           <p className="sec-title-content">לחצו על הקלף!</p>
           <div className="scroll-container">
-  <img
-    src={isOpen ? scrollWithText : closeScroll}
-    alt="scroll"
-    className={`scroll-img 
+            <img
+              src={isOpen ? scrollWithText : closeScroll}
+              alt="scroll"
+              className={`scroll-img 
       ${isOpen ? "scrollWithText fade-in" : "closeScroll floating"}`}
-    onClick={() => setIsOpen(true)}
-  />
-</div>
-<div className="goal-scroll-container">
-  <img src={goal} alt="goal" className="goal-scroll" />
-  <p className="goal-scroll-text">מטרתה- עיצוב הפעולות הערכיות של חיילי צה"ל.</p>
-</div>
+              onClick={() => setIsOpen(true)}
+            />
+          </div>
+          {isOpen && (
+            <div className="goal-scroll-container">
+              <img
+                src={goal}
+                alt="goal"
+                className={`goal-scroll ${!isOpenGoal ? "pulse" : ""}`}
+                onClick={() => setIsOpenGoal(true)}
+              />
+              <div
+                className={`goal-scroll-text-con ${isOpenGoal ? "show" : ""}`}
+              >
+                <p className="goal-scroll-text">
+                  מטרתה- עיצוב הפעולות הערכיות של חיילי צה"ל.
+                </p>
+              </div>
+            </div>
+          )}
+        </div>
+      )}
+
+{page === 2 && (
+        <div className="page3">
+          <p className="title-content">חלקי המגילה</p>
+          <p className="text-content">המגילה מחולקת ל-15 פסקאות שכל פסקה מתמקדת ברכיב אחר בזהות צה"ל:</p>
+          <p className="sec-title-content">לחצו על הפסקאות להשלמת הפאזל!</p>
+          <Pazzle/>
         </div>
       )}
 
@@ -91,8 +115,13 @@ function IDFscroll({ page, setPage, goToPrevSubject }) {
         <img
           src={nextBtn}
           alt="next"
-          className="nextBtn nav-btns"
-          onClick={handleNext}
+          className={`nextBtn nav-btns ${
+            page === 1 && !isOpenGoal ? "disabled" : ""
+          }`}
+          onClick={() => {
+            if (page === 1 && !isOpenGoal) return;
+            handleNext();
+          }}
         />
       </div>
     </div>
